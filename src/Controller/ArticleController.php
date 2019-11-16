ae48bb99485cdb3e03eadf1f47e3f3a8e6fbbc38
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2019-11-16 14:58:17 +0300

    cache

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
@@ -22,18 +23,18 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug,MarkdownInterface $markdown)
+    public function show($slug,MarkdownInterface $markdown,AdapterInterface $cache)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
-        $articleContent=<<<EOF
-        Spicy ** jalapeno bacon ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident beef ribs aute enim veniam ut cillum pork chuck picanha. 
-        Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow turkey shank eu pork belly meatball non cupim.
+        $articleContent= <<<EOF
+        Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident beef ribs aute enim veniam ut cillum pork chuck picanha. 
+        Dolore reprehenderit **labore** minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow turkey shank eu pork belly meatball non cupim.
         
-        Laboris [beef ribs](http://baconipsum.com/) fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip.
+        Laboris [beef ribs](https://baconipsum.com/) fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip.
          Pariatur laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin cupim officia. 
          Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon pork belly ut ullamco.
           Irure velit turducken ground round doner incididunt occaecat lorem meatball prosciutto quis strip steak.
@@ -43,7 +44,15 @@ class ArticleController extends AbstractController
         cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
         fugiat.
 EOF;
-        $articleContent=$markdown->transform($articleContent);
+
+
+        $item=$cache->getItem('markdown_'.md5($articleContent));
+        if (!$item->isHit())
+        {
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+        $articleContent=$item->get();
 
 
         return $this->render('article/show.html.twig', [
